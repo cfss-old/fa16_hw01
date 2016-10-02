34912b1f46a5cfa87df66d194bf4f9cfa028e716
Emily Forden <emilyaforden@gmail.com>
2016-10-02 15:46:05 -0500

    After a break, I tried starting from the beginning.

---
+++ Emily Forden HW1.R
@@ -0,0 +1,16 @@
+#Introduction
+Hello! My name is *Emily Forden* and I am a **5th Year** in the [History PdD program](https://history.uchicago.edu/) where I study ancient Roman economics.
+_Here are some interesting things about me!_
+1. I got married in June!
+2. I have a cat named Otho, who was a Roman Emperor. Here is a photo of him:
+  ![alt text](http://i.imgur.com/maQQvXy.jpg)
+3. When I'm not studying, you can find me scuba diving! I'm working on becoming a dive master.
+
+##Thoughts and Reflections
+Although I found it easy to edit a clone on my computer, I had a really hard time getting
+the document to appear in GitHub. At times, I could be heard muttering:
+  
+  >I'm going to smash my computer
+>like the Hulk
+
+Thankfully, I don't think it will come to that!
